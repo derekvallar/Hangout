a1a8958cffb14ead16100519c23b1fbedb677960
derekvallar <derekvallar@gmail.com>
2016-10-13 17:11:54 -0700

    Initial Commit

---
+++ Hangout/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Hangout
+//
+//  Created by Derek Vitaliano Vallar on 10/13/16.
+//  Copyright © 2016 Derek Vallar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
